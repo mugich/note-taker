7d419786f7c7ac570e3fa0589ba9e121502e3c68
mugi <munkhuu1233@gmail.com>
2021-01-17 19:59:58 -0500

    created server.js, apiNotes.js, htmlRoutes.js files to route html files

---
+++ server.js
@@ -0,0 +1,32 @@
+// ==============================================================================
+// DEPENDENCIES
+var express = require("express");
+
+// EXPRESS CONFIGURATION
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+
+// require("./public/assets/js/apiNotes")(app);
+require("./public/assets/js/htmlRoutes")(app);
+
+
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
