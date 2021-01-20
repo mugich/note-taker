cf727a3294330a5b18860639116571a6a5e0d9bb
mugi <munkhuu1233@gmail.com>
2021-01-19 19:18:52 -0500

    installed node uuid package
    created unique id with delete method

--- routes/apiNotes.js
+++ routes/apiNotes.js
@@ -1,6 +1,8 @@
 // ===============================================================================
 // LOAD DATA
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
 
 // ROUTING
 // ===============================================================================
@@ -12,20 +14,28 @@ module.exports = app => {
 
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) throw err;
-
           jsonData = JSON.parse(data);
           res.json(jsonData);            
       });
       });
-      // API POST Requests
+    //   API POST Requests
       app.post("/api/notes", function(req, res) {
-
+       const id = uuidv4();
+            req.body.id = id;
             jsonData.push(req.body);   
             res.json(true);
-            updateDb();
-                 
-      }); 
-    function updateDb() {
+            updateDb();              
+      });   
+        
+      app.delete("/api/notes/:id", function(req, res) {
+        console.log("req params", req.params.id);
+       jsonData = jsonData.filter(({ id }) => id !== req.params.id);
+        
+        res.json(true);
+        updateDb();
+      });
+
+     function updateDb() {
         fs.writeFile("./db/db.json",JSON.stringify(jsonData,'\t'),err => {
             if (err) throw err;
             return true;
